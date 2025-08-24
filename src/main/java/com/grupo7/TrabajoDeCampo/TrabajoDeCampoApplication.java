961225d53930d2ca1094b2cb0cbe18c1462c286c
OyarzoGabriel <gabriel.oyarzo@alu.frlp.utn.edu.ar>
2025-08-24 16:18:11 -0300

    Subida del SpringBoot

---
+++ src/main/java/com/grupo7/TrabajoDeCampo/TrabajoDeCampoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo7.TrabajoDeCampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoDeCampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoDeCampoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo7/TrabajoDeCampo/TrabajoDeCampoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo7.TrabajoDeCampo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoDeCampoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
